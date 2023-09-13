9661cbb73d87b7703630a9fa3072d63f7d995cf7
root0gama <galliatycommunity@gmail.com>
2023-09-14 01:50:08 +0200

    Navbar.jsx

---
+++ constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "btn#1",
+    title: "btn#1",
+  },
+  {
+    id: "btn#2",
+    title: "btn#2",
+  },
+  {
+    id: "btn#3",
+    title: "btn#3",
+  },
+  {
+    id: "btn#3",
+    title: "btn#3",
+  },
+];
